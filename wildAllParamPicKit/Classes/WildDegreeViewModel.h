0586b09f9dcdb3e6371308b037e3b0cf9e025eec
CP_wangyuetong <woyoto@qq.com>
2019-04-10 09:57:31 +0800

    add pic model

---
+++ wildAllParamPicKit/Classes/WildDegreeViewModel.h
@@ -0,0 +1,18 @@
+//
+//  WildDegreeViewModel.h
+//  Pods-wildAllParamPicKit_Example
+//
+//  Created by Wild on 2019/4/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WildDegreeViewModel : NSObject
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *picUrl;
+@property (nonatomic, copy) NSString *title;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ wildAllParamPicKit/Classes/WildDegreeViewModel.m
@@ -0,0 +1,12 @@
+//
+//  WildDegreeViewModel.m
+//  Pods-wildAllParamPicKit_Example
+//
+//  Created by Wild on 2019/4/10.
+//
+
+#import "WildDegreeViewModel.h"
+
+@implementation WildDegreeViewModel
+
+@end
